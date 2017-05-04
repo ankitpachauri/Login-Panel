5d02a241b20dfe4a249b905a5338995c426f0b7f
Ankit <ankur646@gmail.com>
2017-05-04 22:37:45 +0530

    Login View Created

---
+++ HRMS/src/ConnectionManager.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.sql.*;
+public class ConnectionManager {
+    static Connection con;
+    static String url;
+    
+    public static Connection getConnection(){
+    	try{
+    		String url="jdbc:mysql://localhost:3306/HRMS";
+    		
+    		Class.forName("com.mysql.jdbc.Driver");
+    		try{
+    			con = DriverManager.getConnection(url,"root","root");
+    		}
+    		catch (SQLException e){
+    			e.printStackTrace();
+    		}
+    	}
+    	
+    	catch(ClassNotFoundException ex){
+    		System.out.println(ex);
+    	}
+    	return con;
+    }
+}
